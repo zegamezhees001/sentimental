9b172bb8e19d99f7b0e0d60335d0296fb67eefd9
kungo000 <kungo2009@hotmail.com>
2020-01-11 22:05:22 +0700

    handle member style and remove some thing is not use.

--- src/Team/static/js/member.js
+++ src/Team/static/js/member.js
@@ -1,35 +1,10 @@
+let choose_user = "";
 window.onload = () => {
   const add_btn_team = document.getElementById("add_btn_team");
   add_btn_team.disabled = true;
-  initLoadAttachmentDatas();
   initLoadUserDatas();
 };
 
-function initLoadAttachmentDatas() {
-  const id_att = document.getElementById("id_attachment");
-  const createOption = createElementFun("option", "", "Null Attachment");
-  createOption.value = "null";
-  checkSelectNotNull();
-  id_att.appendChild(createOption);
-  fetch(`${HOSTMAIN}/Team/show_attachments/`)
-    .then(d => d.json())
-    .then(dataCB => {
-      const { data } = dataCB;
-      if (data.length) {
-        removeChild(id_att);
-        checkSelectNotNull();
-        data.forEach(dAtt => {
-          const { name_attachment, id_attachment } = dAtt;
-          const createOption = createElementFun("option", "", name_attachment);
-          createOption.value = id_attachment;
-          id_att.appendChild(createOption);
-        });
-      }
-    })
-    .catch(err => {
-      alert(err);
-    });
-}
 function initLoadUserDatas() {
   const id_user_e = document.getElementById("id_user");
   const createOption = createElementFun("option", "", "Null User");
@@ -58,13 +33,37 @@ function initLoadUserDatas() {
             is_active,
             id_user
           } = dUser;
-          const createOption = createElementFun(
-            "option",
+
+          const createCardDiv = createElementFun("div", "card-user");
+          const createEleNameUser = createElementFun(
+            "h5",
             "",
-            `name: ${first_name} ${last_name}\n status: ${name_attachment} \n employeeID: ${employeeID}`
+            `${first_name} ${last_name}`
           );
-          createOption.value = id_user;
-          id_user_e.appendChild(createOption);
+          const createEleAttachment = createElementFun(
+            "h5",
+            "",
+            `${name_attachment}`
+          );
+          createCardDiv.onclick = () => {
+            const eleCards = document.getElementsByClassName("card-active");
+            if (eleCards.length) {
+              for (let i = 0; i < eleCards.length; i++) {
+                eleCards[i].className = "card-user";
+              }
+            }
+            createCardDiv.className = "card-active";
+            choose_user = id_user;
+            show_update_data(
+              `${first_name} ${last_name}`,
+              name_attachment,
+              employeeID,
+              is_active
+            );
+          };
+          createCardDiv.appendChild(createEleNameUser);
+          createCardDiv.appendChild(createEleAttachment);
+          id_user_e.appendChild(createCardDiv);
         });
       }
     })
@@ -75,14 +74,23 @@ function initLoadUserDatas() {
 
 function checkSelectNotNull() {
   const add_btn_team = document.getElementById("add_btn_team");
-  const id_att = document.getElementById("id_attachment");
-  const id_user = document.getElementById("id_user");
-  if (id_att.value != "null") add_btn_team.disabled = false;
+  if (choose_user) add_btn_team.disabled = false;
   else add_btn_team.disabled = true;
-  if (id_user.value != "null") add_btn_team.disabled = false;
-  else add_btn_team.disabled = true;
+}
+
+function show_update_data(name_user, name_attachment, employeeID, is_active) {
+  checkSelectNotNull();
+  document.getElementById("name_user").innerHTML = `Name user : ${name_user}`;
+  document.getElementById(
+    "name_attachment"
+  ).innerHTML = `Attachment: ${name_attachment}`;
+  document.getElementById(
+    "employeeID"
+  ).innerHTML = `Employee ID: ${employeeID}`;
+  document.getElementById("status_active").innerHTML = `Status: ${is_active}`;
 }
 
 function addToTeam(e) {
   e.preventDefault();
+  console.log({ choose_user });
 }
