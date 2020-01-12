b65065a9384de7652336c153c55396a21b88bc7b
kungo000 <kungo2009@hotmail.com>
2020-01-12 13:53:42 +0700

    refector code

--- src/Users/views_users_admin.py
+++ src/Users/views_users_admin.py
@@ -1,34 +1,10 @@
 from django.shortcuts import render
 from BaseSettings.respone_data import responseData , message_handle
 import json
-from .presenters.user_model import UserModel
+from .presenters.UserModel import UserModel
 from django.contrib.auth.decorators import user_passes_test
 from .models import Profile
 from BaseSettings.permissions import check_user_is_admin
-def zip_data_profiles(profiles):
-    zip_datas = []
-    if len(profiles):
-        for profile in profiles:
-            bd = "" 
-            if (profile.birth_date is not None): 
-                bd = profile.birth_date.strftime("%d/%m/%YT%H:%M:%S") 
-            else: 
-                bd =profile.birth_date
-            print(profile.birth_date is not None , profile.birth_date , bd)
-            data = {
-                "avatar" : str(profile.avatar),
-                "bio" : profile.bio,
-                "tel": profile.tel,
-                "birth_date": bd,
-                "first_name": profile.user.first_name,
-                "last_name": profile.user.last_name,
-                "employeeID": profile.user.employeeID ,
-                "is_staff": profile.user.is_staff ,
-                "is_active": profile.user.is_active ,
-                "id_user": profile.id 
-            }
-            zip_datas.append(data)
-    return zip_datas
 
 
 @user_passes_test(check_user_is_admin, login_url="Users:login")
@@ -52,7 +28,7 @@ def create_new_user(req):
 def user_all(req):
     try:
         profiles = Profile.objects.all()
-        zip_datas = zip_data_profiles(pr ofiles)
+        zip_datas = UserModel().zip_data_profiles(profiles)
         return responseData(message_handle("get data." , zip_datas) , 200)
     except Exception as e:
         print(e)
